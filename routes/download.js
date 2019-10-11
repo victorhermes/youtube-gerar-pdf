c8f17a64c288e8bd3778d6defb8c729f6953853a
Victor <me.victorhermes@gmail.com>
2019-10-10 21:54:59 -0300

    primeiro commit

---
+++ routes/download.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const fs = require("fs");
+var archiver = require("archiver");
+const router = express.Router();
+
+const names = [
+  { filename: "John" },
+  { filename: "Jane" },
+  { filename: "Victor" },
+  { filename: "Joao" },
+  { filename: "Jorge" },
+  { filename: "Paulo" }
+];
+
+router.post("/", (req, res) => {
+  /*var output = fs.createWriteStream(
+    `public/zipFiles/certificate_${Math.random()}.zip`
+  );*/
+
+  var archive = archiver("zip", {
+    zlib: { level: 9 }
+  });
+
+  archive.on("error", function(err) {
+    throw err;
+  });
+
+  // archive.pipe(output) baixa para uma pasta dentro do app
+  archive.pipe(res.attachment("certificate.zip"));
+
+  names.map(n => {
+    archive.file(`public/pdf/${n.filename}.pdf`, {
+      name: `${n.filename}.pdf`
+    });
+  });
+
+  archive.finalize();
+});
+
+module.exports = router;
---
+++ routes/pdf.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+const PDFDocument = require("pdfkit");
+
+const students = [
+  { filename: "John" },
+  { filename: "Jane" },
+  { filename: "Victor" },
+  { filename: "Joao" },
+  { filename: "Jorge" },
+  { filename: "Paulo" }
+];
+
+router.post("/", (req, res) => {
+  try {
+    students.map(s => {
+      const doc = new PDFDocument({
+        layout: "landscape"
+      });
+
+      const pdfStream = fs.createWriteStream(`public/pdf/${s.filename}.pdf`);
+
+      // Stripping special characters
+      //filename = encodeURIComponent(filename) + '.pdf'
+      // Setting response to 'attachment' (download).
+      // If you use 'inline' here it will automatically open the PDF
+      //res.setHeader('Content-disposition', 'inline; filename="' + filename + '"')
+      //res.setHeader('Content-type', 'application/pdf')
+      doc.fontSize(40).text(s.filename, { align: "center" }, 315);
+      doc.image("public/images/cert.png", 10, 35, { width: 770 });
+      doc.pipe(pdfStream);
+      doc.end();
+    });
+  } catch (err) {
+    console.error("ERROR: " + err.message);
+  } finally {
+    res.redirect("/");
+  }
+});
+
+module.exports = router;
